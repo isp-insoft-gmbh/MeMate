6c6a3fb826f40450c96364c4b3e120f2b246a188
Niklas <niklas-weisss@web.de>
2020-01-29 15:51:10 +0100

    Historytable
    changed textalignment, rowheight, grid and headercolor.

--- client/src/main/java/com/isp/memate/History.java
+++ client/src/main/java/com/isp/memate/History.java
@@ -4,13 +4,17 @@
 package com.isp.memate;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.UIManager;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
 /**
  * In der Historie soll der Nutzer alle bisherigen Buchungen (auch von Kollegen) sehen, egal ob etwas
@@ -43,6 +47,7 @@ public class History extends JPanel
   public History()
   {
     super( new BorderLayout() );
+    setBackground( Color.BLUE );
     historyTable = new JTable( ServerCommunication.getInstance().getHistoryData( true ), columnNames );
     tableModel = new DefaultTableModel( ServerCommunication.getInstance().getHistoryData( true ), columnNames )
     {
@@ -54,11 +59,17 @@ public class History extends JPanel
     };
     historyTable.setModel( tableModel );
     historyTable.setAutoCreateRowSorter( true );
-    setBackground( UIManager.getColor( "TabbedPane.highlight" ) );
-    scrollPane.setBackground( UIManager.getColor( "TabbedPane.highlight" ) );
+    historyTable.setShowGrid( false );
+    JTableHeader header = historyTable.getTableHeader();
+    header.setOpaque( false );
+    header.setBackground( header.getBackground().darker() );
+    historyTable.setRowHeight( 30 );
     scrollPane.setBorder( BorderFactory.createEmptyBorder() );
     scrollPane.setViewportView( historyTable );
-    add( scrollPane );
+    add( scrollPane, BorderLayout.CENTER );
+    setBackground( Color.white );
+    scrollPane.setBackground( Color.white );
+    scrollPane.getViewport().setBackground( Color.white );
   }
 
 
@@ -74,5 +85,13 @@ public class History extends JPanel
       }
     };
     historyTable.setModel( tableModel );
+    historyTable.getColumnModel().getColumn( 0 ).setHeaderRenderer( new HorizontalAlignmentHeaderRenderer( SwingConstants.CENTER ) );
+    DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+    centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+    historyTable.getColumnModel().getColumn( 0 ).setCellRenderer( centerRenderer );
+    historyTable.getColumnModel().getColumn( 1 ).setCellRenderer( centerRenderer );
+    historyTable.getColumnModel().getColumn( 2 ).setCellRenderer( centerRenderer );
+    historyTable.getColumnModel().getColumn( 3 ).setCellRenderer( centerRenderer );
+    historyTable.getColumnModel().getColumn( 4 ).setCellRenderer( centerRenderer );
   }
 }
