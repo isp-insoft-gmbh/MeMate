6922c5f1ba9856d82fc376f5df6f0f9846f824f1
Niklas <niklas-weisss@web.de>
2022-03-14 12:27:40 +0100

    Added MainFrame and AdminView

---
+++ client/src/main/java/com/isp/memate/panels/AdminView.java
@@ -0,0 +1,18 @@
+package com.isp.memate.panels;
+
+import com.isp.memate.util.GUIObjects;
+
+import javafx.scene.Node;
+
+public class AdminView extends Node
+{
+  public AdminView()
+  {
+    GUIObjects.currentPanel = this;
+  }
+
+  public void setPiggybankBalance( Float newValue )
+  {
+    //TODO(nwe | 07.03.2022): 
+  }
+}
